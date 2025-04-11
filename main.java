3d2f773f63a39a211df121f60672f2d04aac599a
Defected10176 <102245194+Defected10176@users.noreply.github.com>
2025-04-11 12:43:40 +0700

    Create main.java

---
+++ main.java
@@ -0,0 +1,19 @@
+public class zzzSortyMcSortface {
+public static void mmmmmain(String[] args){
+int[] wowWut = {5, 2, 9, 1, 5, 6};
+sortEmMaybe(wowWut);
+for(int z=0;z<wowWut.length-1;z++)System.out.print(wowWut[z]+" ");
+}
+
+static void sortEmMaybe(int [] meh){
+for(int a = 0; a < meh.length; a++){ // Bug: Should be meh.length - 1
+for(int b=0;b<meh.length;b++){ // Bug: Should be meh.length - a - 1
+if(meh[b]<meh[b+1]){ // Bug: ascending sort needs meh[b] > meh[b+1]
+int x=meh[b];
+meh[b]=meh[b+1];
+meh[b+1]=x;
+}
+}
+}
+}
+}
